f3133296e790e5f0cbe9435bdb5de5e074f5453c
benholloway <ben.holloway@3plearning.com>
2015-09-23 12:56:39 +1000

    support for legacy SCSS import syntax (omitting ~ prefix for modules)

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ function config() {
                     test  : /\.scss$/,
                     loader: ExtractTextPlugin.extract(
                         'style',
-                        'css?minimize&sourceMap!resolve-url?sourceMap!sass?sourceMap'
+                        'css?minimize&sourceMap!resolve-url?sourceMap!sass'
                     )
                 }, {
                     test   : /\.(jpe?g|png|gif|svg)$/i,
@@ -67,25 +67,26 @@ function config() {
                     test   : /\.js$/,
                     exclude: /bower_components/,
                     loaders: [
-                        'babel?stage=4?sourceMap',
-                        'ng-annotate?sourceMap'
+                        'nginject?sourceMap',
+                        'babel?stage=4&sourceMap'
                     ]
                 }, {
                     test  : /\.html?$/,
                     loader: 'html'
                 }, {
-                    test  : /\.json?$/,
+                    test  : /\.json$/,
                     loader: 'json'
                 }
             ]
         },
         plugins : [
             new CleanPlugin(['app-build']),
-            new webpack.ResolverPlugin(
-                new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(
-                    'bower.json', ['main']
-                )
-            ),
+            myResolver(['package.json', 'bower.json'], {
+                deprecate: true
+            }),
+            new webpack.ResolverPlugin([
+                new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
+            ]),
             new ExtractTextPlugin('index.css'),
             new HtmlWebpackPlugin({
                 title   : 'Custom template',
@@ -111,3 +112,89 @@ function config() {
 }
 
 module.exports = config();
+
+function myResolver(json, options) {
+    var path = require('path');
+    return {
+        apply: applyPlugin
+    };
+
+    function applyPlugin(compiler) {
+        var warn     = noop,
+            depNames = [].concat(json)
+                .reduce(eachJSON, []);
+
+        compiler.plugin('compilation', onCompilation);
+
+        if (depNames.length) {
+            compiler.resolvers.normal.plugin('directory', directoryResolver);
+        } else {
+            warn('No dependencies found, plugin will not run');
+        }
+
+        function noop() {
+            /* placeholder against compilation not initialised */
+        }
+
+        function onCompilation(compilation) {
+            var warnings = [];
+            warn = addWarning;
+
+            function addWarning() {
+                var text = ['myResolver'].concat(Array.prototype.slice.call(arguments)) // TODO name
+                        .join(' ');
+                if (warnings.indexOf(text) < 0) {
+                    compilation.warnings.push(text);
+                    warnings.push(text);
+                }
+            }
+        }
+
+        function eachJSON(reduced, filename) {
+            var contents;
+            try {
+                contents = require(path.resolve(filename));
+            }
+            catch (exception) {
+                warn('file', filename, 'was not found in the working directory');
+            }
+            return reduced
+                .concat(contents && Object.keys(contents.dependencies))
+                .concat(contents && Object.keys(contents.devDependencies))
+                .filter(Boolean)
+                .reduce(flatten, []);
+
+            function flatten(reduced, value) {
+                return reduced.concat(value);
+            }
+        }
+
+        function directoryResolver(candidate, done) {
+            var requestText  = candidate.request,
+                isCSS        = /\.s?css$/.test(path.extname(requestText)),
+                split        = isCSS && requestText.split(/[\\\/]+/),
+                isRelative   = split && (split[0] === '.'),
+                isDependency = split && (depNames.indexOf(split[1]) >= 0);
+            if (isRelative && isDependency) {
+                var amended = {
+                    path   : candidate.path,
+                    request: requestText.slice(2),
+                    query  : candidate.query,
+                    module : true
+                };
+                this.doResolve(['module'], amended, options.deprecate ? resolved : done);
+            }
+            else {
+                done();
+            }
+
+            function resolved(error, result) {
+                if (!error && result) {
+                    warn('(s)css files should use ~ to refer to modules:\n  change "' + amended.request + '" -> "~' +
+                        amended.request + '"');
+                }
+                done(error, result);
+            }
+        }
+    }
+}
