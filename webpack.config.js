d45f9ddd9513f6c151f6783f702bbc536a466653
benholloway <ben.holloway@3plearning.com>
2015-10-28 11:58:58 +1100

    minor fix to test regex

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ function config() {
         module       : {
             preloaders: [
                 {
-                    test   : /\.js?$/,
+                    test   : /\.js$/,
                     exclude: './node_modules',
                     loader : 'jshint'
                 }
