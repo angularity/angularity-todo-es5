0b7365b214df734816643f094909d37dd4bb1c8c
benholloway <ben.holloway@3plearning.com>
2015-12-18 15:00:40 +1100

    remove explicit port

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 /* global process:true */
 
 module.exports = require('webpack-angularity-solution')({
-    port    : 55555,
     noApp   : process.env.ANGULARITYTODOES5_NO_APP,
     noTest  : process.env.ANGULARITYTODOES5_NO_TEST,
     noMinify: process.env.ANGULARITYTODOES5_NO_MINIFY,
