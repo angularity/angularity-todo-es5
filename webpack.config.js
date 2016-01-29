60b1b449102e4c05cd8754dd1bd70c76a7da4555
benholloway <ben.holloway@3plearning.com>
2016-01-29 13:24:52 +1100

    add explicit define for jquery

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = require('webpack-angularity-solution')({
     noApp   : process.env.ANGULARITYTODOES5_NO_APP,
     noTest  : process.env.ANGULARITYTODOES5_NO_TEST,
     noMinify: process.env.ANGULARITYTODOES5_NO_MINIFY,
-    release : process.env.ANGULARITYTODOES5_RELEASE
+    release : process.env.ANGULARITYTODOES5_RELEASE,
+    provide : {
+        $              : 'jquery',
+        jQuery         : 'jquery',
+        'window.jQuery': 'jquery'
+    }
 });
