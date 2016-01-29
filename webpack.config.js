3a4a064ba6e84b6ac3fd61a893d02658fa655056
Ben Holloway <github@bholloway.com>
2016-01-29 12:55:29 +1100

    integrated karma-angularity-solution for unit testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /* global process:true */
 
 module.exports = require('webpack-angularity-solution')({
+    port    : process.env.ANGULARITYTODOES5_PORT ? parseInt(process.env.ANGULARITYTODOES5_PORT) : undefined,
     noApp   : process.env.ANGULARITYTODOES5_NO_APP,
     noTest  : process.env.ANGULARITYTODOES5_NO_TEST,
     noMinify: process.env.ANGULARITYTODOES5_NO_MINIFY,
