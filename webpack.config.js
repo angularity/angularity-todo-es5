05b46e567911141731502faa9c6fda317f846b65
benholloway <ben.holloway@3plearning.com>
2016-02-05 16:26:56 +1100

    using latest webpack-angularity-solution, karma-angularity-solution

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 /* global process:true */
 
 module.exports = require('webpack-angularity-solution')({
-    port    : process.env.PORT ? parseInt(process.env.PORT) : undefined,
-    noApp   : process.env.NO_APP,
-    noTest  : process.env.NO_TEST,
-    noMinify: process.env.NO_MINIFY,
-    release : process.env.RELEASE,
-    provide : {
+    globals: {
         $              : 'jquery',
         jQuery         : 'jquery',
         'window.jQuery': 'jquery'
     }
-});
+})
+    .resolve(function () {
+        /* jshint validthis:true */
+        return (process.env.MODE in this) ? this[process.env.MODE] : [].concat(this.app).concat(this.test);
+    });
