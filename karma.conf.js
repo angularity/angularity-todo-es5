7067dd1178c72f63d5fe352e0fec3184c0f37092
benholloway <ben.holloway@3plearning.com>
2016-02-08 11:46:02 +1100

    support new karma-angularity-solution

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,3 @@
 /* global process:true */
 
-module.exports = require('karma-angularity-solution')({
-    port    : process.env.PORT ? (parseInt(process.env.PORT) + 1) : undefined,
-    reporter: process.env.KARMA_REPORTER,
-    browser : process.env.KARMA_BROWSER,
-    logLevel: process.env.KARMA_LOGLEVEL
-});
+module.exports = require('karma-angularity-solution')(process.env);
